ea2a1143b9bad94a7e5db6461eec094ee98ce491
Rogelio Gutierrez <gutierrez.rog12@gmail.com>
2023-02-23 11:15:10 -0300

    feat: CRUD products - create

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,39 @@
-export class CreateProductDto {}
+import { IsArray, IsEnum, IsInt, IsNumber, IsOptional, IsPositive, IsString, MinLength } from 'class-validator'
+
+enum Gender {
+  MEN = 'men',
+  WOMEN = 'women',
+  KID = 'kid',
+  UNISEX = 'unisex',
+}
+
+export class CreateProductDto {
+  @IsString()
+  @MinLength(1)
+  title: string
+
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
+  price?: number
+
+  @IsString()
+  @IsOptional()
+  desc?: string
+
+  @IsString()
+  @IsOptional()
+  slug?: string
+
+  @IsInt()
+  @IsPositive()
+  @IsOptional()
+  stock?: number
+
+  @IsString({ each: true })
+  @IsArray()
+  sizes: string[]
+
+  @IsEnum(Gender)
+  gender: string
+}
